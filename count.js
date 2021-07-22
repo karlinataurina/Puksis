d50ce66ae4ebf73fe15afb1ca8f672e3e51f896d
karlinataurina <karlina.taurina@gmail.com>
2021-07-22 15:48:49 +0300

    22.07.2021.

--- count.js
+++ count.js
@@ -2,33 +2,32 @@
 var countDownDate = new Date("May 1, 2022 10:00:00").getTime();
 
 // lai apdeido laiku ik pēc sekundes
-var x = setInterval(function() {
+var x = setInterval(function () {
 
-// get šodienas datumu un laiku
-var now = new Date().getTime();
-    
-// atrodam attālumu starp tagadējo laiku un laiku līdz kuram skaitīt
-var distance = countDownDate - now;
-    
-// laika aprēķins dienām, stundām, minūtēm un sekundēm
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-// izdod rezultātu elementā ar id="demo"
-document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-+ minutes + "m " + seconds + "s ";
-    
-// kad skaitīšana beigusies, uzrakstām tekstu 
-if (distance < 0)
-{
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "Happy birthday!!!";
-    document.getElementById("hide").style.display = "none";
-    var pic = document.getElementById("Puksis");
-    pic.src = "images/webPuksis.jpg";
-}
+    // get šodienas datumu un laiku
+    var now = new Date().getTime();
+
+    // atrodam attālumu starp tagadējo laiku un laiku līdz kuram skaitīt
+    var distance = countDownDate - now;
+
+    // laika aprēķins dienām, stundām, minūtēm un sekundēm
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // izdod rezultātu elementā ar id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // kad skaitīšana beigusies, uzrakstām tekstu 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Happy birthday!!!";
+        document.getElementById("hide").style.display = "none";
+        var pic = document.getElementById("Puksis");
+        pic.src = "images/webPuksis.jpg";
+    }
 }, 1000);
 
 
@@ -43,14 +42,16 @@ const addVisitor = async () => {
 
     try {
         const response = await fetch('http://130.61.147.255:5000/create_visitor/' + name + '/' + last_name + '/' + age, {
-         method: 'POST',
-         });
-         const data = await response.json();
-      // enter your logic when the fetch is successful
-         console.log(data);
-       } catch(error) {
-     // enter your logic for when there is an error (ex. error toast)
+            method: 'POST',
+        });
+        const data = await response.json();
+        // enter your logic when the fetch is successful
+        console.log(data);
+    } catch (error) {
+        // enter your logic for when there is an error (ex. error toast)
 
-          console.log(error)
-         } 
+        console.log(error)
     }
+
+    document.getElementById("frm1").style.display = "none";
+}
