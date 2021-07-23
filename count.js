a62de904f1ecaf7ac5643db7742e87620ad76d4e
karlinataurina <karlina.taurina@gmail.com>
2021-07-23 16:20:09 +0300

    23.07.2021.

--- count.js
+++ count.js
@@ -41,7 +41,7 @@ const addVisitor = async () => {
     var age = document.getElementById("age").value
 
     try {
-        const response = await fetch('https://130.61.147.255:5000/create_visitor/' + name + '/' + last_name + '/' + age, {
+        const response = await fetch('https://apex.oracle.com/pls/apex/karlinataurina/puksis/create_visitor?firstname=' + name + '&lastname=' + last_name + '&age=' + age, {
             method: 'POST',
         });
         const data = await response.json();
